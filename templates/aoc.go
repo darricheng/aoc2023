f10ca2f503fce45ec95350e8ee087e685ae57a1f
Darric Heng <darricheng@gmail.com>
2024-01-04 14:28:42 +0800

    update template to not use last empty line after linebreak

--- templates/aoc.go
+++ templates/aoc.go
@@ -13,9 +13,14 @@ func main() {
 	}
 	input := string(b)
 	data := strings.Split(input, "\n")
+	numOfLines := len(data)
 
 	for i, line := range data {
+		if i == numOfLines-1 {
+			break
+		}
 		fmt.Println("line", i, "--", line)
+
 		// Code goes here
 
 	}
