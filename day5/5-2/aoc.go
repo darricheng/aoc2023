a93c313820765b98304f3f24e8680e09eadc90c5
Darric Heng <darricheng@gmail.com>
2024-01-13 21:43:09 +0800

    add details of algo

--- day5/5-2/aoc.go
+++ day5/5-2/aoc.go
@@ -8,13 +8,21 @@ import (
 	"strings"
 )
 
+/*
+1. Slice of length 2 arrays, each array is [start, range].
+2. At each step (e.g. changing from soil to water), there are two sub-steps.
+3. Split & transform. For each array, split and transform the numbers according to the rules for that step. This step can be done in parallel.
+4. Sort & merge. Sort the new slice of arrays from smallest to biggest according to the start number. Merge any overlapping arrays.
+5. Repeat steps 3-4 until the location numbers are found. Take slice[0][0] to get the smallest location.
+*/
+
 type mapNums struct {
 	destStart   int
 	sourceStart int
 	rangeLen    int
 }
 
-var seeds []int
+var seedRanges [][2]int // slice of arrays. where each array is [start. range]
 var seedToSoil []mapNums
 var soilToFertilizer []mapNums
 var fertilizerToWater []mapNums
@@ -48,6 +56,7 @@ func main() {
 		case 0: // seeds
 			seedRangesStr := strings.Split(line, " ")[1:] // discard first val which is "seeds:"
 			var seedRangesNum []int
+			// Convert strings to int
 			for _, s := range seedRangesStr {
 				n, err := strconv.Atoi(s)
 				if err != nil {
@@ -56,10 +65,9 @@ func main() {
 				seedRangesNum = append(seedRangesNum, n)
 			}
 			for j := 0; j < len(seedRangesNum); j += 2 {
-				end := seedRangesNum[j] + seedRangesNum[j+1]
-				for k := seedRangesNum[j]; k < end; k++ {
-					seeds = append(seeds, k)
-				}
+				s := seedRangesNum[j]
+				r := seedRangesNum[j+1]
+				seedRanges = append(seedRanges, [2]int{s, r})
 			}
 		case 1: // seed-to-soil
 			currentMap = &seedToSoil
@@ -114,33 +122,68 @@ func main() {
 		}
 	}
 
-	// find the location for each seed
-	for _, seedNum := range seeds {
-		fmt.Printf("Processing seed: %d\n", seedNum)
-		soilNum := process(seedNum, seedToSoil)
-		fertilizerNum := process(soilNum, soilToFertilizer)
-		waterNum := process(fertilizerNum, fertilizerToWater)
-		lightNum := process(waterNum, waterToLight)
-		tempNum := process(lightNum, lightToTemp)
-		humidityNum := process(tempNum, tempToHumidity)
-		locationNum := process(humidityNum, humidityToLocation)
-
-		if locationNum < res {
-			res = locationNum
-		}
-	}
+	soilRanges := process(seedRanges, seedToSoil)
+	fertilizerRanges := process(soilRanges, soilToFertilizer)
+	waterRanges := process(fertilizerRanges, fertilizerToWater)
+	lightRanges := process(waterRanges, waterToLight)
+	tempRanges := process(lightRanges, lightToTemp)
+	humidityRanges := process(tempRanges, tempToHumidity)
+	locationRanges := process(humidityRanges, humidityToLocation)
 
 	fmt.Printf("FINAL RESULT: %d\n", res)
 }
 
 // based on the provided map m, returns the correct destination number for the provided n
-func process(n int, maps []mapNums) int {
-	for _, m := range maps {
-		start := m.sourceStart
-		end := m.sourceStart + m.rangeLen - 1
-		if n >= start && n <= end {
-			return n - start + m.destStart
-		}
-	}
-	return n
+func process(n [][2]int, maps []mapNums) [][2]int {
+	/*
+		# Split and transform
+
+		We loop through the list of ranges, applying every map to it to transform the numbers.
+		If the range gets split in half, we append one half to the end of the ranges slice
+		and continue with the other half.
+		Once we go through all the maps for a range, any leftover range will just be the same
+
+		There are 6 different possibilities for each range to map pair:
+
+		1,2. The range is completely to the left or right of the map
+		range: |-----|    or    |----|
+		map:            |-----|
+		3,4. The range intersects the map at the left or right edge
+		range: |-----|  or  |--------|
+		map:      |-------------|
+		5. The range is a subset of the map
+		range:      |---|
+		map:     |-----------|
+		6. The range is a superset of the map
+		range: |-------------|
+		map:        |-----|
+
+		For ranges that fall right at the edge of the map, go by the following thinking
+
+		3,4. There is at least one value that falls outside one side of the map
+		6. There is at least one value that falls outside each side of the map
+
+		For each possibility, the steps are subtly different
+		1,2. We skip the map entirely
+		3,4. Shrink the range, then append the transformed range to the slice of transformed ranges
+		5. Transform the entire range, continue with the next range in the list
+		6. Append the transformed range, add the rear to the list of ranges to transform,
+		   continue with the front
+
+		If the range goes through all the maps and there is leftover range, add it to
+		the slice of transformed ranges
+
+		The list of ranges will be treated as a queue, as in case 6,
+		we enqueue another pair to check later
+	*/
+
+	/*
+		# Sort and merge
+
+		From split and transform, we should end up with a new slice of slices that
+		are `[start, range]`.
+		However, there might be some ranges that overlap(?), so we sort the ranges by their start,
+		then merge any overlapping ranges together.
+	*/
+
 }
